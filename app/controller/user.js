2a56c994f9657628b005f6ddaf9538f6fca8706e
cheesekun <cheesekun1997@gmail.com>
2019-01-10 17:53:36 +0800

    user bcrypt

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Controller = require('egg').Controller
+const bcrypt = require('bcryptjs')
 
 class UserController extends Controller {
   // 获取自己
@@ -26,11 +27,34 @@ class UserController extends Controller {
     if (!user) {
       ctx.helper.error(ctx, -1, '该账户不存在')
     } else {
-      if (password !== user.password) {
+      if (!bcrypt.compareSync(password, user.password)) {
         ctx.helper.error(ctx, -1, '密码错误')
       } else {
         let token = await ctx.service.token.createToken({ role: 'user', id: user.Id })
         ctx.helper.success(ctx, { token })
+      }
+    }
+  }
+
+  async register () {
+    const ctx = this.ctx
+    // FIXME: 尝试添加手机验证码
+    const { username, password } = ctx.request.body
+
+    let user = await ctx.model.User.getUserByName(username)
+    if (user) {
+      ctx.helper.error(ctx, -1, '该用户名已存在')
+    } else {
+      let hash = await ctx.service.user.encrypt(password)
+      // FIXME: 数据存入数据库
+      let res = await ctx.model.User.create({
+        username,
+        password: hash
+      })
+      if (res) {
+        ctx.helper.success(ctx, {})
+      } else {
+        ctx.helper.error(ctx, -1, '注册失败')
       }
     }
   }
--- app/service/user.js
+++ app/service/user.js
@@ -1,11 +1,25 @@
 // 构造出info的那一堆数据
 'use strict'
 
+const bcrypt = require('bcryptjs')
+
 const Service = require('egg').Service
 
 class UserService extends Service {
   async getMe (token) {
 
+  }
+
+  // 加密密码（注册用）
+  async encrypt (psd) {
+    // 生成salt的迭代次数
+    const saltRounds = 10
+    // 随机生成salt
+    const salt = bcrypt.genSaltSync(saltRounds)
+    // 获取hash值
+    const hash = bcrypt.hashSync(psd, salt)
+
+    return hash
   }
 }
 
